41f5739c1a96f42ae2ffec766d80ac198b47a788
Jays-T <j.j.h.traas@gmail.com>
2020-03-31 15:14:12 +0000

    worked on creating my own jasmine tests, causing my code to break and then fixing it

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,36 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
-    } else {
-        return "Error!";
-    }
-    
+Calculator = function() {
+    this.value = 0;
 }
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == "number") {
+    this.value += number;
+}else{
+    alert("Error!");
+}
+}
+
+whatCanIDrink = function(age) {
+    if (age < 0) {
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    else if (age < 14) {
+        return "Drink Toddy";
+    }
+    else if (age < 18) {
+        return "Drink Coke";
+    }
+    else if (age < 21) {
+        return "Drink Beer";
+    }
+    else if (age < 130) {
+        return "Drink Whisky";
+    }
+    else {
+        return  "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+}
+
+
+ 
+   
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,18 +1,76 @@
 
 
 describe("Calculator", function() {
+
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+
     describe("Addition tests", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add (20);
+            calc.add (22);
+            expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add (7);
+            calc.add (19);
+            expect(calc.value).toBe(26);
         });
         it("should return an error if we don't supply two numbers", function() {
-            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        spyOn(window, "alert");
+        calc.add("Hitchhikers", "Guide");
+        expect(window.alert).toHaveBeenCalledWith("Error!");
         });
-        it("should return 15", function() {
-            expect(addition("5, 5, 5")).toBe(15);
+    });
+});
+
+describe("My whatCanIDrink", function() {
+
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+
+    describe("checks age", function() {
+
+         it("should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        });
+
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
+        });
+        
+        
+        it("should return drink coke when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(17)
+            expect(result).toBe("Drink Coke");
+        });
+        
+        it("should return drink beer when called as whatCanIDrink(18)", function() {
+            var result = whatCanIDrink(18)
+            expect(result).toBe("Drink Beer");
+        });
+        
+        it("should return drink beer when called as whatCanIDrink(20)", function() {
+            var result = whatCanIDrink(20)
+            expect(result).toBe("Drink Beer");
+        }); 
+        
+        it("should return drink whisky when called as whatCanIDrink(30)", function() {
+            var result = whatCanIDrink(30)
+            expect(result).toBe("Drink Whisky");
+        });
+        
+        it("should be unable to return a drink when called as whatCanIDrink(140)", function() {
+            var result = whatCanIDrink(140)
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        
+        it("should contain Drink when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(10).indexOf("Drink");
+            expect(result).not.toEqual(-1);
         });
     });
 });
